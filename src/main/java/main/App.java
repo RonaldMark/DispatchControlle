1ead9bfcac922d14c05fa3c7c2c10a5b6ce47d57
Ronald Langat <ronald.langat@switchlinkafrica.co.ke>
2021-11-11 17:17:06 +0300

    Solve Illegal reflective access

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -10,6 +10,9 @@ import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
 import controller.ServiceController;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.json.JSONException;
 import org.json.JSONObject;
 import service.BatteryService;
@@ -51,6 +54,8 @@ public class App extends AbstractVerticle {
     }
 
     public static void main(String[] args) {
+
+        disableAccessWarnings();
 
         Vertx vertx = Vertx.vertx();
 
@@ -158,5 +163,24 @@ public class App extends AbstractVerticle {
                 System.out.println("System failed to start !!" + resp.failed());
             }
         });
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void disableAccessWarnings() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | NoSuchFieldException | NoSuchMethodException | SecurityException | InvocationTargetException ignored) {
+        }
     }
 }
